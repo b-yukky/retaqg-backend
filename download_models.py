baf73c09e058973c242a7c79db822ebda80e6977
Bryan Djafer <62521415+b-yukky@users.noreply.github.com>
2022-12-21 19:55:31 +0900

    fix: download models script

--- download_models.py
+++ download_models.py
@@ -1,8 +1,8 @@
 import os
 
 weights = {
-    "qgna_squad_t5small": "https://drive.google.com/u/0/uc?id=1-vqF9olcYOT1hk4HgNSYEdRORq-OD5CF&export=download&confirm=t&uuid=25bd5b05-9744-4dd5-af2d-13a5dccea64a&at=ACjLJWkydLJdb3o7p-Sf5HmsGm3y:1671542808588",
-    "dist_race_t5small": "https://drive.google.com/u/0/uc?id=1jKdcbc_cPkOnjhDoX4jMjljMkboF-5Jv&export=download&confirm=t&uuid=a29f69f8-dfc7-4ec8-9b36-a4c3c61fc277&at=ACjLJWnFGOEt7fU1yuqTsrPwte98:1671587310557"
+    "multitask-qg-ag.ckpt": "https://drive.google.com/u/0/uc?id=1-vqF9olcYOT1hk4HgNSYEdRORq-OD5CF&export=download&confirm=t&uuid=25bd5b05-9744-4dd5-af2d-13a5dccea64a&at=ACjLJWkydLJdb3o7p-Sf5HmsGm3y:1671542808588",
+    "race-distractors.ckpt": "https://drive.google.com/u/0/uc?id=1jKdcbc_cPkOnjhDoX4jMjljMkboF-5Jv&export=download&confirm=t&uuid=a29f69f8-dfc7-4ec8-9b36-a4c3c61fc277&at=ACjLJWnFGOEt7fU1yuqTsrPwte98:1671587310557"
 }
 
 weights_folder = './aqg_api/weights'
@@ -14,7 +14,7 @@ def main():
     
     for model_name, dlink in weights.items():
         print(f'downloading {model_name} weights...')
-        os.system(f'wget -nc {dlink} -P {weights_folder}')
+        os.system(f'wget -nc "{dlink}" -P {os.path.join(weights_folder, model_name)}')
         print(f'download complete.')
 
 if __name__ == "__main__":
