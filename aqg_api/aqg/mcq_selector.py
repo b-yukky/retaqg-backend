2b6f4f469371f00863b42b0b8a2d279ed290eb5a
Bryan Djafer <62521415+b-yukky@users.noreply.github.com>
2023-02-12 14:15:24 +0900

    deactivate openai generator

--- aqg_api/aqg/mcq_selector.py
+++ aqg_api/aqg/mcq_selector.py
@@ -1,6 +1,6 @@
 from .ml_models.leafbase_generator import LeafBaseMCQGenerator
 from .ml_models.sumbase_generator import SumBaseMCQGenerator
-from .ml_models.open_ai import OpenAIGenerator
+# from .ml_models.open_ai import OpenAIGenerator
 
 from collections import defaultdict
 
@@ -12,7 +12,7 @@ class MCQSelector():
         self.models = {
             'leafQad_base': LeafBaseMCQGenerator() if self.active['leafQad_base'] else None,
             'sumQd_base': SumBaseMCQGenerator() if self.active['sumQd_base'] else None,
-            'text-davinci-003': OpenAIGenerator() if self.active['text-davinci-003'] else None
+            # 'text-davinci-003': OpenAIGenerator() if self.active['text-davinci-003'] else None
         }
     
     def generate_mcq_questions(self, context, model_name, count):
